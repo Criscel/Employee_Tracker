a35c8bbe06ffa53f35c3c86efe1c3879052ab74f
Criscel Agdeppa <cel_agdeppa@yahoo.com>
2021-03-10 03:05:51 +1100

    fixed update employee by manager

--- actionRoute/update.js
+++ actionRoute/update.js
@@ -68,7 +68,7 @@ const empRoles = (programInit) => {
         {
           name: 'UpdatedRole',
           type: "list",
-          message: "Select Employee's Updated Role: (Please ADD ROLE if not available in selection)",
+          message: "Select Employee's Updated Role: (Please ADD ROLE first if not available in selection)",
           choices: roleArr
         }
       ])
@@ -76,31 +76,84 @@ const empRoles = (programInit) => {
 
           const query = `UPDATE employee_tbl
             SET role_id = (SELECT ID FROM role_tbl WHERE role_title = '${answer.UpdatedRole}')
-            WHERE CONCAT(first_name, ' ' ,  last_name) LIKE '%${answer.employee}%'`;
+            WHERE CONCAT(first_name, ' ' ,  last_name) LIKE '%${answer.employee}%';`;
 
-            connection.query(query,);
+          connection.query(query,);
 
-            console.log(`${answer.employee}`, `${answer.UpdatedRole}`);
-            //console.log(query);
-
-            connection.query(`SELECT e.ID, CONCAT(e.first_name, ' ' ,  e.last_name) AS Employee, e.role_id, r.role_title 
+          connection.query(`SELECT e.ID, CONCAT(e.first_name, ' ' ,  e.last_name) AS Employee, e.role_id, r.role_title 
             FROM employee_tbl e
             INNER JOIN role_tbl r
             WHERE e.role_id = r.ID;`,
-            function (err,res) {
-            if (err) throw err;
-            console.log('Employee Role Successfully Updated!');
-            console.table(res);
-            programInit();
+            function (err, res) {
+              if (err) throw err;
+              console.log('Employee Role Successfully Updated!');
+              console.table(res);
+              programInit();
+            })
         })
-      })
     });
   })
 };
 
 //BONUS
-const empManager = () => {
-  console.log("BONUS, TBA")
-};
+const empManager = (programInit) => {
+  const employeeArr = [];
+  const managerArr = [];
 
-module.exports = { empDetails, empRoles, empManager };
+  connection.query(`SELECT ID, CONCAT(first_name, ' ' ,  last_name) AS Employee FROM employee_tbl;`, (err, res) => {
+    if (err) throw err;
+    for (i = 0; i < res.length; i++) {
+      let employee = res[i].Employee;
+      employeeArr.push(employee);
+    }
+
+    connection.query(`SELECT m.id, CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM role_tbl 
+    INNER JOIN employee_tbl m
+    ON role_tbl.id = m.role_id
+    AND role_tbl.role_title LIKE '%Manager%';`,
+        (err, res) => {
+            if (err) throw err;
+            for (i = 0; i < res.length; i++) {
+                let managers = (res[i].manager);
+                managerArr.push(managers);
+            }
+
+    inquirer.prompt([
+      {
+        name: "employee",
+        type: "list",
+        message: "Select which Employee you want to update: ",
+        choices: employeeArr
+      },
+      {
+        name: "manager",
+        type: "list",
+        message: "Select the Updated Managers below: (Please ADD MANAGER first if not available in selection)",
+        choices: managerArr
+      }
+    ])
+    .then((answer) => {
+      const query = `UPDATE employee_tbl 
+      SET manager_id = (SELECT role_id FROM (SELECT role_id FROM employee_tbl WHERE CONCAT(first_name, ' ' ,  last_name) LIKE '%${answer.manager}%') AS B )
+      WHERE CONCAT(first_name, ' ' ,  last_name) LIKE '%${answer.employee}%';`;
+      
+      connection.query(query, );
+
+      connection.query(`SELECT a.ID, a.first_name, a.last_name, a.manager_id, b.manager
+      FROM employee_tbl a
+      INNER JOIN (SELECT role_id, CONCAT  (first_name,  ' ' ,  last_name) AS manager FROM employee_tbl) b
+      WHERE a.manager_id = b.role_id;`, 
+      function  (err, res) {
+        if (err) throw err;
+        console.log('Employee Manager Successfully Updated!');
+        console.table(res);
+        programInit();
+    })
+  })
+  })
+})
+    //console.log("BONUS, TBA")
+  };
+
+  module.exports = { empDetails, empRoles, empManager };
